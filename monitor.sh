9d065fd61753d06ec757dfb99aa77c5db6392821
Frank Junior <frankcbjunior@gmail.com>
2013-07-10 13:52:16 -0300

    escrita do script melhorado para comando mais apropriados

--- monitor.sh
+++ monitor.sh
@@ -2,8 +2,8 @@
 
 echo "usuario: $(whoami)"
 echo "hostname: $(hostname)"
-echo "SO: $(uname -a | cut -d " " -f1)"
-echo "Distribuicao: $(grep "DISTRIB_DESCRIPTION" /etc/lsb-release | cut -d "=" -f2 | sed s/\"//g)"
-echo "memoria: $(echo "scale=2; 3755 / 1000" | bc)G"
+echo "SO: $(uname -o)"
+echo "Distribuicao: $(lsb_release -sd)"
+echo "memoria: $(free -g)G"
 echo "processador: $(grep "model name" /proc/cpuinfo | uniq | cut -d ":" -f2 | sed s/\ //) x$(grep "model name" /proc/cpuinfo | wc -l)"
-echo "arquitetura: $(lscpu | grep "Architecture:" | sed s/\ *//g | cut -d ":" -f2)"
+echo "arquitetura: $(arch)"
